81c351b4950736b746b9834e2a5c4714805f5747
Sirbu Dan <discipler.093@gmail.com>
2020-04-19 16:31:58 +0300

    Added SGE.hpp to include all files of the library

--- include/SGE/SGE.hpp
+++ include/SGE/SGE.hpp
@@ -1,35 +1,23 @@
 // Copyright 2020 Sirbu Dan
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <SGE/SGE.hpp>
-#include <string>
-#include <gtest/gtest.h>
+#ifndef SGE_SGE_HPP
+#define SGE_SGE_HPP
 
-TEST(Hash, Equality) {
-    sge::Hash h1(0x38d1334144987bf4);
-    sge::Hash h2("Hello, world!");
-    sge::Hash h4(h1);
-    auto h5 = sge::Hash(0x38d1334144987bf4);
+#include <SGE/Types.hpp>
+#include <SGE/Hash.hpp>
+#include <SGE/Log.hpp>
+#include <SGE/Application.hpp>
 
-    EXPECT_EQ(h1, h2);
-    EXPECT_EQ(h1, h4);
-    EXPECT_EQ(h1, h5);
-}
-
-TEST(Hash, Conversion) {
-    sge::Hash h1(0x38d1334144987bf4);
-    auto h = static_cast<std::uint64_t>(h1);
-
-    EXPECT_EQ(h1, h);
-}
+#endif //SGE_SGE_HPP
