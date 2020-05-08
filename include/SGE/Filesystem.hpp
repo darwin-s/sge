c0a8298cbbcd95cb0f939c07bb624bc7f445bc12
Sirbu Dan <discipler.093@gmail.com>
2020-05-08 16:46:21 +0300

    Documented the filesystem files

--- include/SGE/Filesystem.hpp
+++ include/SGE/Filesystem.hpp
@@ -19,19 +19,103 @@
 #include <filesystem>
 
 namespace sge {
+/**
+ * \brief Object representing the virtual filesystem
+ *
+ *
+ * This class is used to access the virtual filesystem of a game.
+ * The filesystem consists of zip and 7z archives or directories (for debug builds).
+ * The respective archives are "mounted" in the filesystem and any overlapping files are
+ * overwritten with those from the latest mounted archive. An archive can be mounted as the
+ * root of a filesystem ("/") or as any other folder inside it. The virtual directory does not have
+ * to exist.
+ * \note If the archive extension was not provided, it will be deducted automatically (note that zip archives have more priority than 7z archives).
+ * \note Symlinks are not supported for security reasons.
+ * Usage example:
+ * \code
+ * if (!sge::Filesystem::mount("archive", "/")) {
+ *     // error mounting...
+ * }
+ * std::size_t size = sge::Filesystem::getFileSize("file.txt");
+ * sge::filesystem::unmount("archive");
+ * \endcode
+ */
 class SGE_API Filesystem {
 public:
+    /**
+     * \brief File type
+     *
+     *
+     * Indicates the type of a file.
+     */
     enum class FileType {
-        Regular,
-        Directory,
-        Other
+        Regular, ///< Regular file
+        Directory, ///< Directory
+        Other ///< Other files (device files, etc)
     };
 
+    /**
+     * \brief File Exists
+     *
+     *
+     * Searches the virtual filesystem for a file and returns whether it was found.
+     * \param path Path to the file
+     * \return true if file exists, false otherwise
+     */
     [[nodiscard]] static bool exists(const std::filesystem::path& path);
+
+    /**
+     * \brief Get File Size
+     *
+     *
+     * Returns the size of a virtual file. If the file does not exist or it is empty 0 is returned.
+     * \param path Path to the file
+     * \return Size of the file, or 0 if it doesn't exist or it is empty
+     */
     [[nodiscard]] static std::size_t getFileSize(const std::filesystem::path& path);
+
+    /**
+     * \brief Is File Read-Only
+     *
+     *
+     * Returns whether the file is read-only or not.
+     * \param path Path to the file
+     * \return true if file is read-only or was not found, false otherwise
+     */
     [[nodiscard]] static bool isFileReadOnly(const std::filesystem::path& path);
+
+    /**
+     * \brief Get File Type
+     *
+     *
+     * Returns the type of the virtual file.
+     * \param path Path to the file
+     * \return The type of the file, or FileType::Other if there was a problem
+     * \sa Filesystem::FileType
+     */
     [[nodiscard]] static FileType getFileType(const std::filesystem::path& path);
+
+    /**
+     * \brief Mount archive
+     *
+     *
+     * Mounts an archive into the virtual filesystem. If no extension was provided, then it will be
+     * deducted automatically.
+     * \note When deducting extension the zip archives have a higher priority than the 7z ones.
+     * \param archive The physical archive to be mounted
+     * \param mountPoint The point on which to mount the archive in the virtual filesystem
+     * \return true on success, false otherwise
+     */
     static bool mount(const std::filesystem::path& archive, const std::filesystem::path& mountPoint);
+
+    /**
+     * \brief Unmount archive
+     *
+     *
+     * Unmount a archive from the virtual filesystem.
+     * \note If the archive was mounted without an extension, then it should be unmounted without one as well
+     * \param archive The physical archive to be unmounted
+     */
     static void unmount(const std::filesystem::path& archive);
 };
 }
--- include/SGE/InputFile.hpp
+++ include/SGE/InputFile.hpp
@@ -20,22 +20,135 @@
 #include <filesystem>
 
 namespace sge {
+/**
+ * \brief Object representing a virtual file
+ *
+ *
+ * This object is used to operated with virtual files in the virtual filesystem. It is used for
+ * streaming data from a virtual file.
+ * Usage example:
+ * \code
+ * sge::InputFile f("file.txt");
+ * sge::ByteData dt = f.read(34);
+ * // process data...
+ * \endcode
+ */
 class SGE_API InputFile {
 public:
-    static constexpr std::size_t defaultBufferSize = 65536;
+    static constexpr std::size_t defaultBufferSize = 65536; ///< Default internal buffer size
+
+    /**
+     * \brief Create file
+     *
+     *
+     * Creates a file that is not yet in an opened state.
+     */
     explicit InputFile();
+
+    /**
+     * \brief Create file
+     *
+     *
+     * Creates a file and immediately opens it.
+     * \param path Path to the virtual file
+     * \param bufferSize Internal buffer size
+     */
     explicit InputFile(const std::filesystem::path& path, std::size_t bufferSize = defaultBufferSize);
+
+    /**
+     * \brief Move file
+     *
+     *
+     * Move-constructs the file object.
+     * \param other The other file
+     */
     InputFile(InputFile&& other) noexcept;
     InputFile(const InputFile&) = delete;
+
+    /**
+     * \brief Destroy file
+     *
+     *
+     * Closes the file and then destroys the object.
+     */
     ~InputFile();
+
+    /**
+     * \brief Move file
+     *
+     *
+     * Move-assigns the file
+     * \param other The other file
+     * \return *this
+     */
     InputFile& operator=(InputFile&& other) noexcept;
     InputFile& operator=(const InputFile&) = delete;
+
+    /**
+     * \brief Open file
+     *
+     *
+     * Tries to open the virtual file.
+     * \param path Path to the virtual file
+     * \param bufferSize Size of the internal buffer
+     * \return true on success, false otherwise
+     */
     bool open(const std::filesystem::path& path, std::size_t bufferSize = defaultBufferSize);
+
+    /**
+     * \brief Is File Open
+     *
+     *
+     * Returns whether the file was opened.
+     * \return true if file is open, false otherwise
+     */
     [[nodiscard]] bool isOpen() const;
+
+    /**
+     * \brief Read File Data
+     *
+     *
+     * Reads a number of bytes from the file and returns it as a std::vector.
+     * \param bytes Number of bytes to read
+     * \return the std::vector containing the data.
+     */
     ByteData read(std::size_t bytes);
+
+    /**
+     * \brief End of File
+     *
+     *
+     * Returns whether the end of file was reached.
+     * \return true if EOF was reached, false otherwise.
+     */
     [[nodiscard]] bool eof() const;
+
+    /**
+     * \brief Get position
+     *
+     *
+     * Returns the current position in the file, relative to it's beginning.
+     * \return The byte number at which this handle is set
+     */
     [[nodiscard]] std::size_t tell() const;
+
+    /**
+     * \brief Seek file
+     *
+     *
+     * Seeks to a certain position in the file, relative to the beginning.
+     * \note If seeking fails, then the the position may be in an undefined state.
+     * \param seekPosition Position to seek to.
+     * \return true on success, false otherwise.
+     */
     [[nodiscard]] bool seekg(std::size_t seekPosition);
+
+    /**
+     * \brief Close file
+     *
+     *
+     * Closes the file handle.
+     */
     void close();
 private:
     void* m_handle;
