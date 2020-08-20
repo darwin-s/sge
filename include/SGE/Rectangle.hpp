1db3176a009b84aa5e11cd56c3b806d5e150bfb9
Sirbu Dan <discipler.093@gmail.com>
2020-08-20 14:11:57 +0300

    Added axis-aligned rectangles

---
+++ include/SGE/Rectangle.hpp
@@ -0,0 +1,151 @@
+// Copyright 2020 Dan Sirbu
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef SGE_RECTANGLE_HPP
+#define SGE_RECTANGLE_HPP
+
+#include <SGE/Export.hpp>
+#include <SGE/Vector2.hpp>
+#include <algorithm>
+
+namespace sge {
+/**
+ * \brief Class representing a rectangle
+ *
+ *
+ * This object is used to represent a rectangle with it's origin in int's top left corner.
+ * It is used for computations, as it does not hold any other data with it. The rectangles are
+ * always axis aligned, because they hold the coordinates of the top-left corner and width and height of the
+ * rectangle.
+ * \tparam T Underlying type of coordinates
+ */
+template<typename T> class Rectangle {
+public:
+    /**
+     * \brief Create rectangle
+     *
+     *
+     * Create a rectangle in (0, 0) with 0 width and length.
+     */
+    Rectangle();
+
+    /**
+     * \brief Create rectangle
+     *
+     *
+     * Create a rectangle in a certain position, with a certain size.
+     * \param left Left coordinate
+     * \param top Top coordinate
+     * \param width Width of rectangle
+     * \param height Height of rectangle
+     */
+    Rectangle(T left, T top, T width, T height);
+
+    /**
+     * \brief Create rectangle
+     *
+     *
+     * Create a rectangle in a certain position, with a certain size.
+     * \param position Rectangle position
+     * \param size Rectangle size
+     */
+    Rectangle(const Vector2<T>& position, const Vector2<T>& size);
+
+    /**
+     * \brief Convert rectangle
+     *
+     *
+     * Convert from rectangle containing another type.
+     * \tparam U Underlying type of the other rectangle
+     * \param rect The rectangle
+     */
+    template<typename U> explicit Rectangle(const Rectangle<U>& rect);
+
+    /**
+     * \brief Rectangle contains point
+     * \param x X coordinate of point
+     * \param y Y coordinate of point
+     * \return true if point is contained, false otherwise
+     */
+    bool contains(T x, T y) const;
+
+    /**
+     * \brief Rectangle contains point
+     * \param point Point position
+     * \return true if point is contained, false otherwise
+     */
+    bool contains(const Vector2<T>& point) const;
+
+    /**
+     * \brief Rectangle intersects rectangle
+     * \param rect The other rectangle
+     * \return true if rectangles intersect, false otherwise
+     */
+    bool intersects(const Rectangle<T>& rect) const;
+
+    /**
+     * \brief Get rectangle intersection
+     *
+     *
+     * Returns the intersection rectangle between two rectangles.
+     * \param rect The other rectangle
+     * \return Intersection rectangle
+     */
+    Rectangle<T> getIntersection(const Rectangle<T>& rect) const;
+
+    /**
+     * \brief Get position
+     * \return Rectangle position
+     */
+    Vector2<T> getPosition() const;
+
+    /**
+     * \brief Get size
+     * \return Rectangle size
+     */
+    Vector2<T> getSize() const;
+
+    /**
+     * \brief Compare rectangles
+     *
+     *
+     * Compares the coordinates of two rectangles.
+     * \param other The other rectangle
+     * \return true if the coordinates match, false otherwise
+     */
+    bool operator==(const Rectangle<T>& other) const;
+
+    /**
+     * \brief Compare rectangles
+     *
+     *
+     * Compares the coordinates of two rectangles.
+     * \param other The other rectangle
+     * \return true if the coordinates DO NOT match, false otherwise
+     */
+    bool operator!=(const Rectangle<T>& other) const;
+
+    T left; ///< Left coordinate
+    T top; ///< Top coordinate
+    T width; ///< Width of rectangle
+    T height; ///< Height of rectangle
+};
+
+#include <SGE/Rectangle.inl>
+
+using RectangleInt = Rectangle<int>;
+using RectangleFloat = Rectangle<float>;
+}
+
+#endif//SGE_RECTANGLE_HPP
---
+++ include/SGE/Rectangle.inl
@@ -0,0 +1,111 @@
+// Copyright 2020 Dan Sirbu
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+template<typename T> Rectangle<T>::Rectangle()
+    : left(static_cast<T>(0)), top(static_cast<T>(0)), width(static_cast<T>(0)), height(static_cast<T>(0)) {
+}
+
+template<typename T> Rectangle<T>::Rectangle(T left, T top, T width, T height)
+    : left(left), top(top), width(width), height(height) {   
+}
+
+template<typename T> Rectangle<T>::Rectangle(const Vector2<T>& position, const Vector2<T>& size)
+    : left(position.x), top(position.y), width(size.x), height(size.y) {
+}
+
+template<typename T>
+template<typename U>
+Rectangle<T>::Rectangle(const Rectangle<U>& rect)
+    : left(static_cast<T>(rect.left)), top(static_cast<T>(rect.top)), width(static_cast<T>(rect.width)),
+      height(static_cast<T>(rect.height)) {
+}
+
+template<typename T> bool Rectangle<T>::contains(T x, T y) const {
+    T minX = std::min(left, left + width);
+    T maxX = std::max(left, left + width);
+    T minY = std::min(top, top + height);
+    T maxY = std::max(top, top + height);
+
+    return (x >= minX) && (x < maxX) && (y >= minY) && (y < maxY);
+}
+
+template<typename T> bool Rectangle<T>::contains(const Vector2<T>& point) const {
+    return contains(point.x, point.y);
+}
+
+template<typename T> bool Rectangle<T>::intersects(const Rectangle<T>& rect) const {
+    T r1MinX = std::min(left, left + width);
+    T r1MaxX = std::max(left, left + width);
+    T r1MinY = std::min(top, top + height);
+    T r1MaxY = std::max(top, top + height);
+
+    T r2MinX = std::min(rect.left, rect.left + rect.width);
+    T r2MaxX = std::max(rect.left, rect.left + rect.width);
+    T r2MinY = std::min(rect.top, rect.top + rect.height);
+    T r2MaxY = std::max(rect.top, rect.top + rect.height);
+
+    T intersLeft   = std::max(r1MinX, r2MinX);
+    T intersTop    = std::max(r1MinY, r2MinY);
+    T intersRight  = std::min(r1MaxX, r2MaxX);
+    T intersBottom = std::min(r1MaxY, r2MaxY);
+
+    if (intersLeft < intersRight && intersTop < intersBottom) {
+        return true;
+    }
+
+    return false;
+}
+
+template<typename T> Rectangle<T> Rectangle<T>::getIntersection(const Rectangle<T>& rect) const {
+    if (!intersects(rect)) {
+        return Rectangle<T>(0, 0, 0, 0);
+    }
+
+    T r1MinX = std::min(left, left + width);
+    T r1MaxX = std::max(left, left + width);
+    T r1MinY = std::min(top, top + height);
+    T r1MaxY = std::max(top, top + height);
+
+    T r2MinX = std::min(rect.left, rect.left + rect.width);
+    T r2MaxX = std::max(rect.left, rect.left + rect.width);
+    T r2MinY = std::min(rect.top, rect.top + rect.height);
+    T r2MaxY = std::max(rect.top, rect.top + rect.height);
+
+    T intersLeft = std::max(r1MinX, r2MinX);
+    T intersTop = std::max(r1MinY, r2MinY);
+    T intersRight = std::min(r1MaxX, r2MaxX);
+    T intersBottom = std::min(r1MaxY, r2MaxY);
+
+    return Rectangle<T>(intersLeft, intersTop, intersRight - intersLeft, intersBottom - intersTop);
+}
+
+template<typename T> Vector2<T> Rectangle<T>::getPosition() const {
+    return Vector2<T>(left, top);
+}
+
+template<typename T> Vector2<T> Rectangle<T>::getSize() const {
+    return Vector2<T>(width, height);
+}
+
+template<typename T> bool Rectangle<T>::operator==(const Rectangle<T>& other) const {
+    return left == other.left && top == other.top && width == other.width && height == other.height;
+}
+
+template<typename T> bool Rectangle<T>::operator!=(const Rectangle<T>& other) const {
+    return !(*this == other);
+}
+
+
+
+
