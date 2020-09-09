0c611f72e345967ee3d80c91d00e393226dea18b
Sirbu Dan <discipler.093@gmail.com>
2020-09-09 14:35:41 +0300

    Made VBOs use persistent memory mapping

--- include/SGE/VBO.hpp
+++ include/SGE/VBO.hpp
@@ -17,6 +17,7 @@
 
 #include <SGE/Export.hpp>
 #include <SGE/Types.hpp>
+#include <memory>
 
 namespace sge {
 class VAO;
@@ -25,18 +26,45 @@ class VAO;
  * \brief Vertex Buffer Object
  * 
  * 
- * Object representing a vertex buffer. It is used to hold vertex data
- * for drawing.
+ * Object representing a vertex buffer. It is used to hold vertex data.
+ * Vertex buffers have access flags, which restrict their use. If a buffer
+ * is initialized with read access only, then mapping it for writing will
+ * give an error.
  */
 class SGE_API VBO {
 public:
     /**
-     * \brief Construct a new VBO object
-     * 
-     * 
-     * Construct a vertex buffer with no data
+     * \brief Buffer access flags
+     *
+     *
+     * Represents the access intention when mapping the buffer, or
+     * the access restriction when creating it.
+     */
+    enum AccessFlags {
+        NoAccess    = 0,     ///< Buffer may not be accessed after creation
+        ReadAccess  = 1 << 0,///< Buffer may be accessed for reading
+        WriteAccess = 1 << 1 ///< Buffer may be accessed for writing
+    };
+
+    /**
+     * \brief Create vertex buffer
+     *
+     *
+     * Creates a vertex buffer with 0 size, and no data
      */
     VBO();
+
+    /**
+     * \brief Create vertex buffer
+     *
+     *
+     * Creates a vertex buffer of a certain size, and access restrictions,
+     * optionally initializing it's contents, with some data.
+     * \param size Size of the vertex buffer in bytes
+     * \param flags Access flags for the vertex buffer
+     * \param data Optional data to fill the buffer
+     */
+    VBO(std::size_t size, AccessFlags flags, void* data = nullptr);
 
     /**
      * \brief Construct a new VBO object
@@ -63,17 +91,82 @@ public:
     VBO& operator=(const VBO&) = delete;
 
     /**
-     * \brief Set the buffer data
-     * 
-     * 
-     * Sets the data that the VBO will hold
-     * \param size Size of the data
-     * \param data Pointer to the data
+     * \brief Allocate buffer
+     *
+     *
+     * Allocates buffer storage and optionally initializes it with some data.
+     * \note If the buffer is already allocated, then calling this function will give an error
+     * \param size Size of the buffer
+     * \param flags Access flags for the buffer
+     * \param data Optional data to initialize the buffer
      */
-    void setData(std::size_t size, const void* data) const;
+    void allocate(std::size_t size, AccessFlags flags, void* data = nullptr);
+
+    /**
+     * \brief Is buffer allocated
+     *
+     *
+     * Returns whether the buffer storage was allocated.
+     * \return true if buffer was allocated, false otherwise
+     */
+    [[nodiscard]] bool isAllocated() const;
+
+    /**
+     * \brief Get buffer size
+     *
+     *
+     * Returns the storage size of the buffer
+     * \return Storage size
+     */
+    [[nodiscard]] std::size_t getSize() const;
+
+    /**
+     * \brief Map buffer
+     *
+     *
+     * Maps a range of the buffer into CPU memory, to read or write to it.
+     * This mapping is persistent, so it
+     * \note Currently you cannot map a buffer for both reading and writing.
+     * \param offset Offset inside the buffer where the map is going to start
+     * \param length Length of the map
+     * \param flags Access flags for the map
+     * \param invalidateRange Invalidate the previous contents of the buffer in the specified range
+     * \return Pointer to the memory map
+     */
+    [[nodiscard]] void* mapBuffer(std::size_t offset,
+                                  std::size_t length,
+                                  AccessFlags flags,
+                                  bool invalidateRange = false);
+
+    /**
+     * \brief Unmap buffer
+     */
+    void unmapBuffer();
+
+    /**
+     * \brief Flush changes
+     *
+     *
+     * Flushes the changes made to a buffer mapping.
+     * \param offset Offset of the sub-region to be flushed.
+     * \param length Length of the sub-region to be flushed.
+     */
+    void flushChanges(std::size_t offset, std::size_t length);
+
+    /**
+     * \brief Bind as element array
+     *
+     *
+     * Binds the current array as a element index target.
+     */
+    void bindElementArray();
 
 private:
     unsigned int m_id;
+    bool m_allocated;
+    std::size_t m_size;
+    bool m_mapped;
+    std::size_t m_mapLength;
 
     friend class VAO;
 };
--- src/VBO.cpp
+++ src/VBO.cpp
@@ -18,29 +18,130 @@
 #include <glad.h>
 
 namespace sge {
-VBO::VBO() : m_id(0) {
+VBO::VBO()
+    : m_id(0), m_allocated(false), m_size(0), m_mapped(false), m_mapLength(0) {
     assert(Context::getCurrentContext());
     glCreateBuffers(1, &m_id);
 }
 
-VBO::VBO(VBO&& other) noexcept : m_id(other.m_id) {
-    other.m_id = 0;
+VBO::VBO(std::size_t size, AccessFlags flags, void* data)
+    : m_id(0), m_allocated(true), m_size(size), m_mapped(false),
+      m_mapLength(0) {
+    assert(Context::getCurrentContext());
+    glCreateBuffers(1, &m_id);
+    GLbitfield f = 0;
+
+    if (flags != NoAccess) {
+        f |= GL_MAP_PERSISTENT_BIT;
+    }
+
+    if ((flags & ReadAccess) != 0) {
+        f |= GL_MAP_READ_BIT;
+    }
+    if ((flags & WriteAccess) != 0) {
+        f |= GL_MAP_WRITE_BIT;
+    }
+    glNamedBufferStorage(m_id, size, data, f);
+}
+
+VBO::VBO(VBO&& other) noexcept
+    : m_id(other.m_id), m_allocated(other.m_allocated), m_size(other.m_size),
+      m_mapped(other.m_mapped), m_mapLength(other.m_mapLength) {
+    other.m_id        = 0;
+    other.m_allocated = false;
+    other.m_size      = 0;
+    other.m_mapped    = false;
+    other.m_mapLength = 0;
 }
 
 VBO::~VBO() {
     assert(Context::getCurrentContext());
+    unmapBuffer();
     glDeleteBuffers(1, &m_id);
 }
 
 VBO& VBO::operator=(VBO&& other) noexcept {
-    m_id       = other.m_id;
-    other.m_id = 0;
+    m_id              = other.m_id;
+    m_allocated       = other.m_allocated;
+    m_size            = other.m_size;
+    m_mapped          = other.m_mapped;
+    m_mapLength       = other.m_mapLength;
+    other.m_id        = 0;
+    other.m_allocated = false;
+    other.m_size      = 0;
+    other.m_mapped    = false;
+    other.m_mapLength = 0;
 
     return *this;
 }
 
-void VBO::setData(const std::size_t size, const void* data) const {
+void VBO::allocate(std::size_t size, AccessFlags flags, void* data) {
+    assert(!m_allocated && Context::getCurrentContext());
+
+    GLbitfield f = 0;
+
+    if (flags != NoAccess) {
+        f |= GL_MAP_PERSISTENT_BIT;
+    }
+
+    if ((flags & ReadAccess) != 0) {
+        f |= GL_MAP_READ_BIT;
+    }
+    if ((flags & WriteAccess) != 0) {
+        f |= GL_MAP_WRITE_BIT;
+    }
+    glNamedBufferStorage(m_id, size, data, f);
+
+    m_allocated = true;
+    m_size      = size;
+}
+
+bool VBO::isAllocated() const {
+    return m_allocated;
+}
+
+std::size_t VBO::getSize() const {
+    return m_size;
+}
+
+void* VBO::mapBuffer(std::size_t offset,
+                     std::size_t length,
+                     AccessFlags flags,
+                     bool invalidateRange) {
+    assert(m_allocated && Context::getCurrentContext());
+    m_mapped    = true;
+    m_mapLength = length;
+
+    GLbitfield f = GL_MAP_PERSISTENT_BIT | GL_MAP_FLUSH_EXPLICIT_BIT;
+
+    if ((flags & ReadAccess) != 0) {
+        f |= GL_MAP_READ_BIT;
+    } else if ((flags & WriteAccess) != 0) {
+        f |= GL_MAP_WRITE_BIT | GL_MAP_UNSYNCHRONIZED_BIT;
+    }
+
+    if (invalidateRange && (flags & ReadAccess) == 0) {
+        f |= GL_MAP_INVALIDATE_RANGE_BIT;
+    }
+
+    return glMapNamedBufferRange(m_id, offset, length, f);
+}
+
+void VBO::unmapBuffer() {
     assert(Context::getCurrentContext());
-    glNamedBufferData(m_id, size, data, GL_STATIC_DRAW);
+    if (m_mapped) {
+        glFlushMappedNamedBufferRange(m_id, 0, m_mapLength);
+        glUnmapNamedBuffer(m_id);
+    }
+}
+
+void VBO::flushChanges(std::size_t offset, std::size_t length) {
+    assert(m_mapped && Context::getCurrentContext());
+    glFlushMappedNamedBufferRange(m_id, offset, length);
+}
+
+void VBO::bindElementArray() {
+    assert(Context::getCurrentContext());
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_id);
 }
 }
